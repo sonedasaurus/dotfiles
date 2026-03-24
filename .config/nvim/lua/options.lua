868928d5701a1843aa7fc42a4be6c5ca6f246b8c
takuya soneda <10826536+sonedasaurus@users.noreply.github.com>
2026-03-24 21:41:31 +0900

    少しずつ必要なプラグインを設定

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,6 +6,7 @@ vim.o.number = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 
 -- Space as leader key
 vim.g.mapleader = vim.keycode('<Space>')
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,115 @@
+-- ========================================================================== --
+-- ==                               PLUGINS                                == --
+-- ========================================================================== --
+
+vim.api.nvim_create_autocmd('PackChanged', {
+  desc = 'execute plugin callbacks',
+  callback = function(event)
+    local data = event.data or {}
+    local kind = data.kind or ''
+    local callback = vim.tbl_get(data, 'spec', 'data', 'on_' .. kind)
+
+    if type(callback) ~= 'function' then
+      return
+    end
+
+    -- possible callbacks: on_install, on_update, on_delete
+    local ok, err = pcall(callback, data)
+    if not ok then
+      vim.notify(err, vim.log.levels.ERROR)
+    end
+  end,
+})
+
+vim.pack.add({
+  { src = 'https://github.com/folke/tokyonight.nvim' },
+	{ src = 'https://github.com/neovim/nvim-lspconfig' },
+	{ src = 'https://github.com/VonHeikemen/ts-enable.nvim' },
+	{ src = 'https://github.com/nvim-mini/mini.nvim', version = 'main' },
+	{  
+		src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+    version = 'main',
+    data = {
+      on_update = function()
+        vim.cmd('TSUpdate')
+      end,
+    },
+  },
+})
+
+-- ========================================================================== --
+-- ==                         PLUGIN CONFIGURATION                         == --
+-- ========================================================================== --
+
+vim.cmd.colorscheme('tokyonight-night')
+
+-- Toggle file explorer
+local mini_files = require('mini.files')
+mini_files.setup({})
+
+local map_split = function(buf_id, lhs, direction)
+  local rhs = function()
+    local cur_target = MiniFiles.get_explorer_state().target_window
+    local new_target
+
+    vim.api.nvim_win_call(cur_target, function()
+      vim.cmd(direction .. ' split')
+      new_target = vim.api.nvim_get_current_win()
+    end)
+
+    MiniFiles.set_target_window(new_target)
+    MiniFiles.go_in()
+  end
+
+  vim.keymap.set('n', lhs, rhs, {
+    buffer = buf_id,
+    desc = 'Split ' .. direction,
+  })
+end
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'MiniFilesBufferCreate',
+  callback = function(args)
+    local buf_id = args.data.buf_id
+    map_split(buf_id, 'gs', 'belowright horizontal')
+    map_split(buf_id, 'gv', 'belowright vertical')
+  end,
+})
+
+vim.keymap.set('n', '<leader>e', function()
+  if mini_files.close() then
+    return
+  end
+
+  mini_files.open()
+end, { desc = 'File explorer'} )
+
+require('mini.statusline').setup({})
+
+-- Treesitter setup
+local ts_parsers = {
+  "bash",
+  "dockerfile",
+  "gitignore",
+  "go",
+  "html",
+  "javascript",
+  "json",
+  "kotlin",
+  "lua",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "sql",
+  "swift",
+  "typescript",
+  "vim",
+  "vimdoc",
+  "yaml",
+}
+
+vim.g.ts_enable = {
+  parsers = ts_parsers,
+  auto_install = true,
+  highlights = true,
+}
