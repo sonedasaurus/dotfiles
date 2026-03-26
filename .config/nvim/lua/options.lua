b808239133fecfd85b1cff1e0cf0323f95a0cf1f
Takuya Soneda <10826536+sonedasaurus@users.noreply.github.com>
2026-03-26 17:00:56 +0900

    Add Options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -11,6 +11,12 @@ vim.g.have_nerd_font = false
 -- Make line numbers default
 vim.o.number = true
 
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
