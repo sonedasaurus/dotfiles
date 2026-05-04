7c49155d879aa51edd0281cd3a946917e7301f50
sonedasaurus <10826536+sonedasaurus@users.noreply.github.com>
2026-05-04 22:15:28 +0900

    Rebuild nvim config (#2)
    * 一旦設定を全部削除
    
    * 少しずつ必要なプラグインを設定
    
    * split in file explore
    
    * コメント分割
    
    * signcolumn = yes
    
    * Only open files with Enter in the file explorer
    
    * LSP
    
    * lua_ls
    
    * neo-tree
    
    * markdown preview
    
    * コメント追加
    
    * 見出しの背景を削除
    
    * add github copilot
    
    * Add Options
    
    * Remove option
    
    * コメント修正
    
    * timeの設定
    
    * listchars
    
    * optionsの変更
    
    * カスタムcss

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,20 +1,6 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- Disable netrw because the file tree plugin handles directory browsing instead.
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
-
--- [[ Setting options ]]
 require 'options'
-
--- [[ Basic Keymaps ]]
-require 'keymaps'
-
--- [[ Install `lazy.nvim` plugin manager ]]
-require 'bootstrap'
-
--- [[ Configure and install plugins ]]
 require 'plugins'
